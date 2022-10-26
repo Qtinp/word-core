b05ac34039711be9f5bc14c3ceb35211415b041c
BSTluo <1946831552@qq.com>
2022-10-26 14:04:22 +0800

    集中配置文件:   Function/Config/index.ts 	modified:   index.ts

--- index.ts
+++ index.ts
@@ -2,12 +2,13 @@ import Editor from './Editor/index'
 import Driver from './Driver'
 import fs from 'fs'
 import path from 'path'
+import { config } from './Function/Config'
 
-const dir = path.join(__dirname, './data')
-const editor = new Editor(dir)
+const dir = config.dir
+export const editor = new Editor(dir)
 
 const cacheObj = editor.getCacheWord()
-const driver = new Driver(cacheObj, dir)
+export const driver = new Driver(cacheObj, dir)
 
 // 导出外部的主动触发函数
 export const initiative = (q: string, playData: { [key: string]: any }) => { driver.initiativeStart(q, playData) }
@@ -17,4 +18,3 @@ export const initiative = (q: string, playData: { [key: string]: any }) => { dri
 fs.readdirSync(path.join(__dirname, './Function/initiative/')).forEach((value) => {
     require(path.join(__dirname, './Function/initiative/', value))
 })
-

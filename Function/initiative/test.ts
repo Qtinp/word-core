be3a7378042863872577638a26691fb080a18674
BSTluo <1946831552@qq.com>
2022-11-03 15:40:46 +0800

    modified:   Function/initiative/test.ts 	modified:   Permissions/index.ts

--- Function/initiative/test.ts
+++ Function/initiative/test.ts
@@ -1,7 +1,6 @@
 import { initiative } from '../../index'
 
-/*
+
 setInterval(() => {
   console.log(initiative('每隔2秒', {}))
 }, 2000)
-*/
--- Permissions/index.ts
+++ Permissions/index.ts
@@ -1,5 +1,3 @@
-import * as fs from 'fs'
-import * as path from 'path'
 import * as api from '../Tools/index'
 let dir:string
 
